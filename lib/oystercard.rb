759f40b9c73b8e99e059356df1907ac3e1909baa
limcuili <limcuili@hotmail.com>
2018-07-18 14:22:52 +0100

    Q10 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,37 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
+  MAX_BALANCE = 90
+  MIN_BALANCE = 1
+  MIN_FARE = 2.30
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-    fail 'Error: Card cannot exceed £90' if (@balance + amount > 90)
+    fail "Error: Card cannot exceed £#{MAX_BALANCE}" if (@balance + amount > MAX_BALANCE)
     @balance += amount
+    @balance.round(2)
+  end
+
+  def touch_in
+    fail 'Error: Card has insufficient balance' if (@balance < MIN_BALANCE)
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MIN_FARE)
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+private
+  def deduct(amount)
+    @balance -= amount
+    @balance.round(2)
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,6 +11,10 @@ describe Oystercard do
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
+    it 'ensures that we top up to 2 decimal places' do
+      expect(subject.top_up(1.999999)).to eq 2
+    end
+
     it 'gives balance 10 when top_up(10) is called on a new card' do
       # within an 'it' block, the first call for subject gives Oystercard.new
       # after that, it is considered an instance.
@@ -22,15 +26,80 @@ describe Oystercard do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by(1)
     end
 
+    max_balance = Oystercard::MAX_BALANCE
+
     it 'raises an error when top_up(100) is called on a new card' do
-      expect{ subject.top_up(100) }.to raise_error 'Error: Card cannot exceed £90'
+      expect{ subject.top_up(max_balance + 10) }.to raise_error "Error: Card cannot exceed £#{max_balance}"
     end
 
     it 'raises an error when we top up over the £90 limit on an existing card' do
-      subject.top_up(80)
-      subject.top_up(10)
-      expect{ subject.top_up(1) }.to raise_error 'Error: Card cannot exceed £90'
+      subject.top_up(max_balance)
+      expect{ subject.top_up(1) }.to raise_error "Error: Card cannot exceed £#{max_balance}"
     end
   end
+
+  # describe '#deduct' do
+  #   it { is_expected.to respond_to(:deduct).with(1).argument }
+  #
+  #   it 'ensures that we deduct to 2 decimal places' do
+  #     subject.top_up(10)
+  #     expect(subject.deduct(1.99999999999)).to eq 8
+  #   end
+  #
+  #   it 'can deduct £2.90 from an existing card of balance 20' do
+  #     subject.top_up(20)
+  #     expect(subject.deduct(2.90)).to eq (20 - 2.90)
+  #   end
+  #
+  #   it 'changes the balance by a negative amount' do
+  #     subject.top_up(15)
+  #     expect{ subject.deduct(5.17) }.to change{ subject.balance}.by(-5.17)
+  #   end
+  # end
+
+  describe '#touch_in, #in_journey?, #touch_out' do
+    it { is_expected.to respond_to(:touch_in) }
+    it { is_expected.to respond_to(:in_journey?) }
+    it { is_expected.to respond_to(:touch_out) }
+
+    context 'new clean card' do
+      it 'gives an error when a new card of balance 0 is touched in' do
+        expect{ subject.touch_in }.to raise_error 'Error: Card has insufficient balance'
+      end
+
+      it 'is initially not in journey' do
+        expect(subject).not_to be_in_journey
+      end
+    end
+
+    context 'a card with £5 balance' do
+      before do
+        subject.top_up(5)
+      end
+
+      it 'is in journey once we have touched in' do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+
+      it 'is not in journey once we have touched out' do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
+      end
+
+      it 'gives an error when an existing card of balance 0.5 is touched in' do
+        subject.top_up(-4.5)
+        expect{ subject.touch_in }.to raise_error 'Error: Card has insufficient balance'
+      end
+
+      it 'deducts the minimum fare from your card when you touch out' do
+        minimum_fare = Oystercard::MIN_FARE
+        expect{ subject.touch_out }.to change{ subject.balance }.by(-minimum_fare)
+      end
+
+    end
+  end
+
 
 end
